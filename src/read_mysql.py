b61926597c284728f50ab4d621884dd25acf17a6
yaolili <yaolili12235@gmail.com>
2016-07-17 14:51:05 +0800

    add time counter

--- src/read_mysql.py
+++ src/read_mysql.py
@@ -18,20 +18,32 @@ if __name__ == "__main__":
         db.autocommit(True)
         try:
             cur = db.cursor()
+            # sleep_time & flag are used to record continuous sleep time
+            sleep_time = 0
+            flag = False
             while True:
-                cur.execute("SELECT * FROM raw WHERE is_process = 1")
-                if len(cur.fetchall()) == 0:
+                cur.execute("SELECT * FROM raw WHERE is_process = 0")
+                result = cur.fetchall()
+                if len(result) == 0:
+                    print 1
                     time.sleep(5)
+                    if flag:
+                        sleep_time += 5
+                        # if continuous sleep time more than 2 hours, break
+                        if sleep_time > 7200: break
+                    else:
+                        sleep_time = 5
+                        flag = True
                     continue
-                for row in cur.fetchall():
+                flag = False
+                for row in result:
                     id = str(row[0])
                     json_data = row[1]
                     # deal with json_data, remain TO DO
-                    
+                    print id
                     
                     # update database
-                    cur.execute("UPDATE raw SET is_process = 0 WHERE id = %s", (id,))
-                # exit()
+                    cur.execute("UPDATE raw SET is_process = 1 WHERE id = %s", (id,))
         except MySQLdb.Error, e:
             print e
         db.close()
