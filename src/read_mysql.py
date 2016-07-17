3dd4e8b4c91b86c0807c915403c326cb7883dd30
yaolili <yaolili12235@gmail.com>
2016-07-17 10:10:49 +0800

    add read mysql data

---
+++ src/read_mysql.py
@@ -0,0 +1,33 @@
+#-*- coding:utf-8 -*-
+# AUTHOR:   yaolili
+# FILE:     read_mysql.py
+# ROLE:     TODO (some explanation)
+# CREATED:  2016-07-16 20:25:01
+# MODIFIED: 2016-07-16 20:25:03
+
+import MySQLdb
+
+
+if __name__ == "__main__":
+        db = MySQLdb.connect(host="localhost",   
+                             user="root",         
+                             passwd="webkdd",  
+                             db="trec16")        
+
+        db.autocommit(True)
+        try:
+            cur = db.cursor()
+            while True:
+                cur.execute("SELECT * FROM raw WHERE is_process = 1")
+                for row in cur.fetchall():
+                    id = str(row[0])
+                    json_data = row[1]
+                    # deal with json_data, remain TO DO
+                    
+                    
+                    # update database
+                    cur.execute("UPDATE raw SET is_process = 0 WHERE id = %s", (id,))
+                exit()
+        except MySQLdb.Error, e:
+            print e
+        db.close()
