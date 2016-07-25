42fe313e8298f14174c289e157f89c8365e1346e
ziaoang <ziaoang@gmail.com>
2016-07-25 19:43:17 +0800

    add word2vec again

--- src/package/utils.py
+++ src/package/utils.py
@@ -8,7 +8,22 @@ def load_stopword_set():
     return stopword_set
 
 def load_vector_dict():
-    return {}
+    print("load word2vec ...")
+    absolute_path = "/index15/w2v/w2v.txt"
+    vector_dict = {}
+    line_no = 0
+    for line in open(absolute_path):
+        line_no += 1
+        if line_no == 1:
+            continue
+        t = line.strip().split()
+        word = t[0]
+        vector = []
+        for i in range(1, len(t)):
+            vector.append(float(t[i]))
+        vector_dict[word] = vector
+    print("load word2vec over")
+    return vector_dict
 
 def load_corpus_dict():
     absolute_path = os.path.join(os.path.dirname(__file__) + "/../../data/data15/1507ALL.txt")
