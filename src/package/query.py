0d79d5d4a011273a3e96ca77146b43089bd4c126
ziaoang <ziaoang@gmail.com>
2016-07-27 22:23:42 +0800

    rm advance query

--- src/package/query.py
+++ src/package/query.py
@@ -2,8 +2,8 @@ import json
 from trecjson import TrecJson
 
 class Query(TrecJson):
-    def __init__(self, query_json, stopword_set):
-        TrecJson.__init__(self, stopword_set, {})
+    def __init__(self, query_json, stopword_set, vector_dict):
+        TrecJson.__init__(self, stopword_set, vector_dict)
 
         try:
             t = json.loads(query_json)
@@ -14,7 +14,10 @@ class Query(TrecJson):
             
             self.plain_text   = self.extract_plain_text(self.title)
             self.word_list    = self.extract_word_list(self.plain_text)
+            self.word_distri  = self.extract_distribution(self.word_list)
             self.stem_list    = self.stem(self.filter_stopword(self.word_list))
+            self.stem_distri  = self.extract_distribution(self.stem_list)
+            self.vector       = self.extract_vector(self.word_list)
 
             self.is_valid = True
         except:
