72d82661029300896433eedfb27aadefc058cf0b
ziaoang <ziaoang@gmail.com>
2016-07-27 22:51:55 +0800

    add new tf and w2v for raw

---
+++ src/tf_raw.py
@@ -0,0 +1,27 @@
+from collections import defaultdict
+
+tf = defaultdict(int)
+line_no = 0
+for line in open("/index15/preprocess/raw_filtered.txt"):
+    line_no += 1
+    if line_no % 100000 == 0:
+        print "%d lines" % line_no
+    t = line.strip().split('\t')
+    for w in t[3].split(' '):
+        tf[w] += 1
+
+total_count = 0
+to_sort = []
+for w in tf:
+    total_count += tf[w]
+    to_sort.append([w, tf[w]])
+to_sort.sort(key=lambda x: x[1], reverse=True)
+
+df = open("../data/tf_raw.dat", "w")
+df.write("%d\n" % total_count)
+for t in to_sort:
+    df.write("%s\t%d\n" % (t[0], t[1]))
+df.close()
+
+
+
