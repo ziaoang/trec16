3ca81d237d2ccaf001ef109d39f3f9f8d791d4fc
ziaoang <ziaoang@gmail.com>
2016-08-01 07:43:09 +0000

    change path

--- src/preprocess/linode_jp.py
+++ src/preprocess/linode_jp.py
@@ -6,7 +6,7 @@ from package.utils import load_stopword_set
 
 stopword_set = load_stopword_set()
 
-df = open("linode_jp.txt", "w")
+df = open("/index15/preprocess/linode_jp.txt", "w")
 
 def single_file(file_path):
     with gzip.open(file_path, 'rt') as f:
@@ -16,11 +16,11 @@ def single_file(file_path):
                 df.write( "%s\t%s\t%s\t%s\n" % (t.created_at, t.id_str, " ".join(t.word_list), " ".join(t.stem_list)) )
 
 def multi_files():
-    for day in range(25, 31):
+    for day in range(25, 29):
         for hour in range(24):
             print "%02d\t%02d" % (day, hour)
             try:
-                file_path = "/home/qiangrw/twitter-tools/twitter-tools-core/statuses.log.2016-07-%02d-%02d.gz"%(day, hour)
+                file_path = "/home/ziaoang/linode_jp/statuses.log.2016-07-%02d-%02d.gz"%(day, hour)
                 single_file(file_path)
                 print "OK"
             except Exception, e:
