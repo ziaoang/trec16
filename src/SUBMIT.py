5a19e98f4e03256cd3ac3c798d9cf5ceb0f571c3
ziaoang <ziaoang@gmail.com>
2016-08-01 19:37:10 +0800

    add submit

---
+++ src/SUBMIT.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import time
+import json
+import MySQLdb
+import datetime
+
+def main():
+    try:
+        conn = MySQLdb.connect(host   = 'localhost',
+                               user   = 'root',
+                               passwd = 'webkdd',
+                               db     = 'trec16',
+                               port   = 3306)
+        cur=conn.cursor()
+
+        cur.execute('SELECT * FROM submit WHERE is_process = 0 limit 10')
+        rows = cur.fetchall()
+        for row in rows:
+            id            = row[0]
+            qid           = row[1]
+            tid           = row[2] 
+            client_id     = row[3]
+            
+            order = "curl -X POST -H 'Content-Type: application/json' 54.164.151.19:80/tweet/%s/%s/%s -s -w %{http_code}" % (qid, tid, client)
+            responce = os.popen(order).read()
+            if responce == '204':
+                cur.execute('UPDATE submit SET is_process = 1 WHERE id = %d' % id)
+            
+            time.sleep(1)
+
+        conn.commit()
+    
+        cur.close()
+        conn.close()
+    
+        print 'process %d rows' % len(rows)
+    except Exception, e:
+        print e
+
+if __name__ == "__main__":
+    while True:
+        print "main ..."
+        main()
+        
+        print 'sleep 10 seconds ...'
+        time.sleep(10)
+
+
+
